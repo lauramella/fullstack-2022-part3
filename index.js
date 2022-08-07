f10b3c8492fd2beb235b1d4bc1650830ed892c46
Laura Mella <laura.e.mella@student.jyu.fi>
2022-08-07 16:28:56 +0300

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny :method :url :status :res[content-length] - :response-time ms :postContact'))
 
@@ -81,6 +83,7 @@ const person = {
     response.status(204).end()
   })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
